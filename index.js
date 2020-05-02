b31a7b8509e2fd259b8fc81b7278c4ab1307b56a
hudaman0621 <mrhu828@gmail.com>
2020-05-02 13:34:54 -0400

    fixed auto display watchlist

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 const allMovies = document.querySelector('.all-movies')
 const movieList = document.getElementById('search-form');
 
-document.addEventListener('DOMContentLoaded', function () {
-    function renderMovies(movieData) {
-        let movieHtmlArray = movieData.map(function (currentMovie) {
-            return ` 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div class="card w-100 h-100">
-                    <img class="card-img-top w-100 p-3" src="${currentMovie.Poster}"  alt="Card image cap"></img>
-                    <div class="card-body">
-                        <h5 class="card-title">${currentMovie.Title}</h5>
-                        <p class="card-text">${currentMovie.Year}</p>
-                        <a href='#/' id='trig1'>Trailer </a>
-                        <iframe id='ifrm1' name='ifrm1' &autoplay=1; frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" src='${currentMovie.Intro}' width="200" height="200" ></iframe>
-                        <a href="#" class="btn btn-primary p-2" onclick="saveToWatchlist('${currentMovie.imdbID}')" >Add to Favorite</a>
-                    </div>
-                </div>
-            </div>
-        `
-        });
-        console.log(movieHtmlArray);
-        return movieHtmlArray.join('');
-    }
+// document.addEventListener('DOMContentLoaded', function () {
+//     function renderMovies(movieData) {
+//         let movieHtmlArray = movieData.map(function (currentMovie) {
+//             return ` 
+//             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+//                 <div class="card w-100 h-100">
+//                     <img class="card-img-top w-100 p-3" src="${currentMovie.Poster}"  alt="Card image cap"></img>
+//                     <div class="card-body">
+//                         <h5 class="card-title">${currentMovie.Title}</h5>
+//                         <p class="card-text">${currentMovie.Year}</p>
+//                         <a href="#" class="btn btn-primary p-2" onclick="saveToWatchlist('${currentMovie.imdbID}')" >Add to Favorite</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         `
+//         });
+//         console.log(movieHtmlArray);
+//         return movieHtmlArray.join('');
+//     }
 
-});
+// });
 
 const submit = document.querySelector('#search-form')
 $(document).ready(function () {
@@ -37,8 +35,6 @@ $(document).ready(function () {
                     <div class="card-body">
                         <h5 class="card-title">${currentMovie.Title}</h5>
                         <p class="card-text">${currentMovie.Year}</p>
-                        <a href='#/' id='trig1'>Trailer </a>
-                        <iframe id='ifrm1' name='ifrm1' &autoplay=1; frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" src='${currentMovie.Intro}' width="200" height="200" ></iframe>
                         <a href="#" class="btn btn-primary p-2" onclick="saveToWatchlist('${currentMovie.imdbID}')" >Add to Favorite</a>
                     </div>
                 </div>
