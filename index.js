3d86c7e8d0654145cae105eadedbda3df3cd90a0
hudaman0621 <mrhu828@gmail.com>
2020-05-02 13:25:12 -0400

    cleaned console.log on index.js

--- index.js
+++ index.js
@@ -25,13 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-// const searchFn =document.getElementById('search-bar')
-// movieList.addEventListener("submit", function (e) {
-//     e.preventDefault();
-// });
-
-
-// console.log('======================');
 const submit = document.querySelector('#search-form')
 $(document).ready(function () {
     // Get value on button click and show alert
@@ -76,39 +69,10 @@ $(document).ready(function () {
 
     });
 });
-// console.log(str); //can i still get the information here?  -nope, it's inside the function
 
-
-http://www.omdbapi.com/?apikey=b43843a0&s=S(${submit}) 
-
-// const searchString = document.getElementById('search-bar').value
-// http://www.omdbapi.com/?apikey=3430a78&s=SearchStringGoesHere
-
-
-// // urlEncodedSearchString = encodeURIComponent()
-// const urlEncodedSearchString = encodeURIComponent(searchString);
-
-
-// const $submitBtn = $('submit')  //declare submitBtn 
-// const $input = $('search-bar') //input value from search-bar
-// $submitBtn.click(function () {    //click function when submit button is clicked
-
-//     var searchString = document.getElementById('search-bar').value //searchString = value from search-bar
-//     const newValue = searchString.val() //creates a newValue to hold the information from $field.val()
-//     $item.text(newValue); //puts the newValue into text and name it $item
-//     console.log(newValue);//print newValue
-//     searchString.append($item) //append item to searchString. 
-// })
-
-// console.log(searchString);
-console.log('!!!!!!!!!!!!!!!!!!!!!!');
-
-
-// let movies = [];
 function saveToWatchlist(imdbID) {
-    console.log(imdbID);
-    // const movie = movieData.find(currentMovie => currentMovie.imdbID == imdbID);
-    // console.log(movie);
+    // console.log(imdbID);
+
     axios.get("http://www.omdbapi.com/?apikey=efe3c50b&i=" + imdbID)
     .then(function (response) {
         const movie = response.data
@@ -122,36 +86,7 @@ function saveToWatchlist(imdbID) {
         watchlistJSON = JSON.stringify(watchlist);
         // localStorage.set('key',1);
         localStorage.setItem('.watch-movies', watchlistJSON);
-        console.log(watchlistJSON);
-        console.log(watchlist);
+        // console.log(watchlistJSON);
+        // console.log(watchlist);
     });
 };
-// Create the variable movie with const movie =
-// Set it equal to movieData.find();
-// Give the find method an anonymous function that takes currentMovie as a parameter
-// Have this anonymous function return currentMovie.imdbID == imdbID;
-// In the end, you’ll have
-// const movie = movieData.find(function (currentMovie) {
-//     return currentMovie.imdbID == imdbID;
-// });
-
-
-// addFavorite.addEventListener('click', function () {
-//     console.log(currentMovie.imdbID)
-        // addFavorite.textContent = "Generating Doggo...";
-
-            // .then(function (data) {
-
-            // console.log(currentMovie[2])
-                // const img = document.createElement('img');
-                // img.setAttribute('src', data.message);
-
-
-                // const imagesDiv = document.getElementById('images');
-                // imagesDiv.innerHTML = '';
-                // imagesDiv.appendChild(img);
-
-                // dogButton.textContent = 'Generate Doggo';
-            // });
-    // });
-// })
